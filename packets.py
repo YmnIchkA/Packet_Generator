e40e9ab5d156b831d84897d02c2f89e416b440b6
Maksim <Makarov2717@gmail.com>
2020-11-24 21:11:39 +0300

    returns only entered values

--- packets.py
+++ packets.py
@@ -1,97 +1,96 @@
-from tkinter import StringVar
+from tkinter import StringVar, IntVar
 
 
 class IPHeader:
 
     def __init__(self) -> None:
         self.version = 4
-        self.ihl = StringVar()
-        self.tos = StringVar()
-        self.len = StringVar()
-        self.id = StringVar()
-        self.flags = StringVar()
-        self.frag = StringVar()
-        self.ttl = StringVar()
-        self.proto = StringVar()
-        self.chksum = StringVar()
-        self.src = StringVar()
-        self.dst = StringVar()
+        self.ihl = IntVar(value=0)
+        self.tos = IntVar(value=0)
+        self.len = IntVar(value=0)
+        self.id = IntVar(value=0)
+        self.flags = StringVar(value='0')
+        self.frag = IntVar(value=0)
+        self.ttl = IntVar(value=0)
+        self.proto = StringVar(value='0')
+        self.chksum = IntVar(value=0)
+        self.src = StringVar(value='0')
+        self.dst = StringVar(value='0')
 
     def packet(self):
-        return {'version': self.version,
-                'ihl': self.ihl,
-                'tos': self.tos,
-                'len': self.len,
-                'id': self.id,
-                'flags': self.flags,
-                'frag': self.frag,
-                'ttl': self.ttl,
-                'proto': self.proto,
-                'chksum': self.chksum,
-                'src': self.src,
-                'dst': self.dst}
+        class_dict = self.__dict__
+        return_dict = dict()
+        for key in class_dict:
+            if class_dict[key] == 0 or class_dict[key] == '0':
+                continue
+            return_dict[key] = class_dict[key]
+
+        return return_dict
 
 
 class UDPHeader:
 
     def __init__(self) -> None:
-        self.sport = StringVar()
-        self.dport = StringVar()
-        self.len = StringVar()
-        self.chksum = StringVar()
-        self.name = StringVar()
-        self.data = StringVar()
+        self.sport = IntVar(value=0)
+        self.dport = IntVar(value=0)
+        self.len = IntVar(value=0)
+        self.chksum = IntVar(value=0)
+        self.name = StringVar(value='0')
+        self.data = StringVar(value='0')
 
     def packet(self):
-        return {'source port': self.sport,
-                'destination port': self.dport,
-                'len': self.len,
-                'chksum': self.chksum,
-                'data': self.data}
+        class_dict = self.__dict__
+        return_dict = dict()
+        for key in class_dict:
+            if class_dict[key] == 0 or class_dict[key] == '0':
+                continue
+            return_dict[key] = class_dict[key]
+
+        return return_dict
 
 
 class TCPHeader:
 
     def __init__(self) -> None:
-        self.sport = StringVar()
-        self.dport = StringVar()
-        self.seq = StringVar()
-        self.ack = StringVar()
-        self.dataofs = StringVar()
-        self.flags = StringVar()
-        self.window = StringVar()
-        self.chksum = StringVar()
-        self.urgptr = StringVar()
-        self.options = StringVar()
-        self.data = StringVar()
-        self.name = StringVar()
+        self.sport = IntVar(value=0)
+        self.dport = IntVar(value=0)
+        self.seq = IntVar(value=0)
+        self.ack = IntVar(value=0)
+        self.dataofs = IntVar(value=0)
+        self.flags = StringVar(value='0')
+        self.window = IntVar(value=0)
+        self.chksum = IntVar(value=0)
+        self.urgptr = IntVar(value=0)
+        self.options = StringVar(value='0')
+        self.data = StringVar(value='0')
+        self.name = StringVar(value='0')
 
     def packet(self):
-        return {'sport': self.sport,
-                'dport': self.dport,
-                'seq': self.seq,
-                'ack': self.ack,
-                'dataofs': self.dataofs,
-                'flags': self.flags,
-                'window': self.window,
-                'chksum': self.chksum,
-                'urgptr': self.urgptr,
-                'options': self.options}
+        class_dict = self.__dict__
+        return_dict = dict()
+        for key in class_dict:
+            if class_dict[key] == 0 or class_dict[key] == '0':
+                continue
+            return_dict[key] = class_dict[key]
+
+        return return_dict
 
 
 class ICMPHeader:
     def __init__(self) -> None:
-        self.type = StringVar()
-        self.code = StringVar()
-        self.chksum = StringVar()
-        self.id = StringVar()
-        self.seq = StringVar()
-        self.name = StringVar()
+        self.type = StringVar(value='0')
+        self.code = IntVar(value=0)
+        self.chksum = IntVar(value=0)
+        self.id = IntVar(value=0)
+        self.seq = IntVar(value=0)
+        self.name = StringVar(value='0')
 
     def packet(self):
-        return{'type': self.type,
-               'code': self.code,
-               'chksum': self.chksum,
-               'id': self.id,
-               'seq': self.seq}
+        class_dict = self.__dict__
+        return_dict = dict()
+        for key in class_dict:
+            if class_dict[key] == 0 or class_dict[key] == '0':
+                continue
+            return_dict[key] = class_dict[key]
 
+        return return_dict
